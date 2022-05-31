439ccd3d2140cd9bd04b5af14c8771f3b1de404f
dan-serr <79541413+dan-serr@users.noreply.github.com>
2022-06-01 00:38:19 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:lappenultima_app/theme/theme.dart';
+
+void main() {
+  runApp(const Appenultima());
+}
+
+class Appenultima extends StatelessWidget {
+  const Appenultima({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'lappenultima',
+      theme: AppenultimaTheme.light(),
+      darkTheme: AppenultimaTheme.dark(),
+      themeMode: ThemeMode.system,
+      home: const Login(),
+    );
+  }
+}
---
+++ lib/screens/login.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  late TextEditingController _userController;
+  late TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    _userController = TextEditingController();
+    _passwordController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('lappenultima')
+      ),
+    )
+  }
+
+}
